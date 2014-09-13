02d4a0eaf6bcbd1ee000c9d5e7f196b614eea4f5
Michiel <code@m01.eu>
2014-09-13 17:28:01 +0100

    Added static file dir

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -81,6 +81,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
