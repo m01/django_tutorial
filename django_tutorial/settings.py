6ae9b0a463125f39ade7cba87e9351cbbb1d70e0
Michiel <code@m01.eu>
2014-09-13 17:31:03 +0100

    STATIC_ROOT was what I actually wanted to set...

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -81,9 +81,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
